84c3797838334a6b6da9f44cf0d3bdb3ff25b625
Jeremy <jerimielid7@gmail.com>
2025-04-30 11:36:34 +0300

    Addition of Factorials and Fibonacci

--- Recursion.py
+++ Recursion.py
@@ -24,5 +24,25 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-for numbers in range(12):
-        print(f'{numbers}: {fibonacci(numbers)} ')
+#for numbers in range(int(input("Which number are you fibing??: "))):
+#       print(f'{numbers}: {fibonacci(numbers)} ')
+
+
+# A function is independent of an object/variable but a method tends to depend on one
+fcache = {} #dictionary
+def fib(n):
+    if n in fcache:
+        return fcache[n]
+
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 2
+    elif n>2:
+        value = fib(n-1)+fib(n-2)
+
+    fcache[n] = value
+    return value
+
+for n in range(1, int(input("What value is your maximum: "))):
+        print(f"{fib(n)}\n")
